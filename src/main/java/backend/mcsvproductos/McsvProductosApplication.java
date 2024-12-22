1131f14849ab7b0c1272512388d33c6281820f0a
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2024-12-22 11:03:00 -0500

    se creo microservicio de productos

---
+++ src/main/java/backend/mcsvproductos/McsvProductosApplication.java
@@ -0,0 +1,13 @@
+package backend.mcsvproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvProductosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsvProductosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/backend/mcsvproductos/McsvProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.mcsvproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvProductosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
