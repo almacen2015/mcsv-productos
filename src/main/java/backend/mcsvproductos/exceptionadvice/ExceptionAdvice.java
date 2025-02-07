0acb47ec0007d4312f912f340bab66a7d918f3ba
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-02-06 19:04:43 -0500

    se agrego para que se hagan logs en un archivo

--- src/main/java/backend/mcsvproductos/exceptionadvice/ExceptionAdvice.java
+++ src/main/java/backend/mcsvproductos/exceptionadvice/ExceptionAdvice.java
@@ -1,6 +1,7 @@
 package backend.mcsvproductos.exceptionadvice;
 
 import backend.mcsvproductos.exceptions.ProductoException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Objects;
 
+@Slf4j
 @ControllerAdvice
 public class ExceptionAdvice {
 
@@ -17,8 +19,10 @@ public class ExceptionAdvice {
                 Objects.equals(e.getMessage(), ProductoException.PRODUCTO_NOMBRE_VACIO) ||
                 Objects.equals(e.getMessage(), ProductoException.PRODUCTO_PRECIO_INVALIDO) ||
                 Objects.equals(e.getMessage(), ProductoException.ID_INVALIDO)) {
+            log.error(e.getMessage(), e);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } else {
+            log.error(e.getMessage(), e);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
