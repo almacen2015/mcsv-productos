331897d362368beb269d1c1e9ef6fc0695451959
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-02-16 11:16:00 -0500

    se agrego spring security

---
+++ src/main/java/backend/mcsvproductos/filter/JwtFilter.java
@@ -0,0 +1,39 @@
+package backend.mcsvproductos.filter;
+
+import backend.mcsvproductos.util.JwtUtils;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+
+    public JwtFilter(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String jwtToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (jwtToken != null) {
+            jwtToken = jwtToken.substring(7);
+            DecodedJWT decodedJWT = jwtUtils.validateToken(jwtToken);
+            String username = jwtUtils.extractUsername(decodedJWT);
+            String stringRoles = jwtUtils.getSpecificClaim(decodedJWT, "authorities").asString();
+        }
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
